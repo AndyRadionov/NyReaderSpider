6193f05c5d3b61926842acdb818442e1ec31e50f
Andrey Radionov <andyomsk@gmail.com>
2017-11-16 22:09:45 +0300

    Icons added

--- app/src/main/java/com/radionov/nyreaderspidertest/ui/MainActivity.java
+++ app/src/main/java/com/radionov/nyreaderspidertest/ui/MainActivity.java
@@ -2,9 +2,12 @@ package com.radionov.nyreaderspidertest.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
 
 import com.radionov.nyreaderspidertest.R;
 import com.radionov.nyreaderspidertest.ui.articles.ArticlesFragment;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.active_fragment, new ArticlesFragment())
                     .commit();
         }
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        ImageView nyTitle = findViewById(R.id.ny_title);
+        Picasso.with(this)
+                .load(R.raw.the_new_york_times_logo)
+                .into(nyTitle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
