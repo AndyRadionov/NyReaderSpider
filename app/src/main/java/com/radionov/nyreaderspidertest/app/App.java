9e6fd0cf82b0e6a5f6cd87a85a24c6f096980a68
Andrey Radionov <andyomsk@gmail.com>
2017-11-16 11:36:19 +0300

    Added Application class, permissions

---
+++ app/src/main/java/com/radionov/nyreaderspidertest/app/App.java
@@ -0,0 +1,26 @@
+package com.radionov.nyreaderspidertest.app;
+
+import android.app.Application;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Application class
+ *
+ * @author Andrey Radionov
+ */
+
+public class App extends Application {
+
+}
---
+++ app/src/main/java/com/radionov/nyreaderspidertest/app/AppPreferences.java
@@ -0,0 +1,15 @@
+package com.radionov.nyreaderspidertest.app;
+
+/**
+ * Class for storing app preferences such as data URL
+ *
+ * @author Andrey Radionov
+ */
+
+public class AppPreferences {
+    public static final String USERS_DATA_URL = "https://api.nytimes.com/svc/news/v3/content/all/";
+    public static final String API_KEY = "61923f85497e45c48b2e3e8d9c0c3998";
+
+    private AppPreferences() {
+    }
+}
