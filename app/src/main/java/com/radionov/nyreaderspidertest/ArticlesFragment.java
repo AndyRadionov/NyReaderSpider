e9bc9dfd578a3560d0d0a4480bae9c36ae532f71
Andrey Radionov <andyomsk@gmail.com>
2017-11-16 11:57:25 +0300

    Added ArticlesFragment

---
+++ app/src/main/java/com/radionov/nyreaderspidertest/ArticlesFragment.java
@@ -0,0 +1,28 @@
+package com.radionov.nyreaderspidertest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass for showing list of ny times articles
+ */
+public class ArticlesFragment extends Fragment {
+
+
+    public ArticlesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_articles, container, false);
+    }
+}
